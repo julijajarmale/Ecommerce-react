c4125b6c6efad73a8126f71653fd5ac256797a1a
Julija Ilginytė-Jarmalė <julija.jarmale@gmail.com>
2022-09-30 12:20:46 +0300

    register and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import './style/Layout.scss';
-import './style/App.scss';
-import './style/Base.scss';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
+import "./style/Layout.scss";
+import "./style/App.scss";
+import "./style/Base.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Women from './pages/Women';
-import Men from './pages/Men';
-import Blog from './pages/Blog';
-import Sale from './pages/Sale';
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
-import Cart from './pages/Cart';
-
+import Women from "./pages/Women";
+import Men from "./pages/Men";
+import Blog from "./pages/Blog";
+import Sale from "./pages/Sale";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes> 
-        <Route path="/" element={<Home/>} />
-        <Route path="/product" element={<Product/>} />
-        <Route path="/productlist" element={<ProductList/>} />
-        <Route path="/women" element={<Women/>} /> 
-        <Route path="/men" element={<Men/>} /> 
-        <Route path="/blog" element={<Blog/>} /> 
-        <Route path="/sale" element={<Sale/>} /> 
-        <Route path="/cart" element={<Cart/>} /> 
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/sale" element={<Sale />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
-);
+  );
 }
 
 export default App;
