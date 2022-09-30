fa232d5dee4d2280cdaaf87fac3e05f14ffb3047
Julija Ilginytė-Jarmalė <julija.jarmale@gmail.com>
2022-09-30 11:58:34 +0300

    slider, data.js

---
+++ src/data.js
@@ -0,0 +1,106 @@
+import slide1 from './assets/slide-1.jpg';
+import slide2 from './assets/slide-2.jpg';
+import slide3 from './assets/slide-3.jpg';
+import boots from './assets/categories/boots.jpg';
+import dress from './assets/categories/dress.jpg';
+import outerwear from './assets/categories/outerwear.jpg';
+import prod1 from './assets/products/prod1.jpg';
+import prod2 from './assets/products/prod2.jpg';
+import prod3 from './assets/products/prod3.jpg';
+import prod4 from './assets/products/prod4.jpg';
+import prod5 from './assets/products/prod5.jpg';
+import prod6 from './assets/products/prod6.jpg';
+import prod7 from './assets/products/prod7.jpg';
+import prod8 from './assets/products/prod8.jpg';
+
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: slide3,
+        title: "Even more than you expected!",
+        desc: "Fashion, Premium, Sneakers, Sportswear and more.",
+       
+    },
+    {
+        id: 2,
+        img: slide2,
+        title: "Discover your brands!",
+        desc: "Discover the brands you follow. Follow brands to be the first to see their news",
+        
+    },
+    {
+        id: 3,
+        img: slide1,
+        title: "Free shipping and returns",
+        desc: "The best things are free",
+        
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: boots,
+        title: "NEW-SZN BOOTS",
+        desc: "Be the baddie",
+        
+       
+    },
+    {
+        id: 2,
+        img: dress,
+        title: "GLAM DRESSES",
+        desc: "Amp it up",
+       
+    },
+    {
+        id: 3,
+        img: outerwear,
+        title: "OUTERWEAR",
+        desc: "Beat-the-chill",
+        
+    },
+];
+
+export const products = [
+    {
+        id: 1,
+        img: prod1,
+    
+    },
+    {
+        id: 2,
+        img: prod2,
+    },
+    {
+        id: 3,
+        img: prod3,
+        
+    }, 
+    
+    {
+        id: 4,
+        img: prod4,
+    
+    },
+    {
+        id: 5,
+        img: prod5,
+    },
+    {
+        id: 6,
+        img: prod6,
+    
+    },
+    {
+        id: 7,
+        img: prod7,
+    },
+    {
+        id: 8,
+        img: prod8,
+    
+    },
+   
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       
       <Navbar/>
+      <Slider/>
     
     </div>
   )
