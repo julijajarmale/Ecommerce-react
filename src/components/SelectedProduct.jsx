a34e1accd88a24c9689e256b03f8499bc8e10c2d
Julija Ilginytė-Jarmalė <julija.jarmale@gmail.com>
2022-10-19 12:59:29 +0300

    css

--- src/components/SelectedProduct.jsx
+++ src/components/SelectedProduct.jsx
@@ -1,44 +1,46 @@
-
-import React from 'react'
+import React from "react";
 
 function SelectedProduct() {
   return (
-    <div className='p-container'>
-    <div className="img-container">
-    <img src={JSON.parse(localStorage.img)} alt="#" />
+    <div className="p-container">
+      <div className="img-container">
+        <img src={JSON.parse(localStorage.img)} alt="#" />
       </div>
-          <div className="info-container">
-            <h1 className='title'>hello</h1>
-            <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores impedit ad sit natus! Possimus in molestiae rerum quis impedit fugiat sint aliquam et! Eligendi harum laborum aperiam explicabo aut quasi.</p>
-            <b className='price'>499.99</b>
-            <div className='filter'>
-            <p>Select color</p>
-            <select>
-              <option>Black</option>
-              <option>Blue</option>
-              <option>Yellow</option>
-              <option>Red</option>
-            </select>
-            <p>Select size</p>
-            <select>
-              <option>XS</option>
-              <option>S</option>
-              <option>M</option>
-              <option>L</option>
-              <option>XL</option>
-            </select>
-            </div>
-            <div className='add'>
-            <p>-</p>
-            <p>1</p>
-            <p>+</p>
-            <div/>
-            
-          </div>
-          <button className="button-1">Add to cart</button>
-          </div>
+      <div className="info-container">
+        <h1 className="title">hello</h1>
+        <p className="desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+          impedit ad sit natus! Possimus in molestiae rerum quis impedit fugiat
+          sint aliquam et! Eligendi harum laborum aperiam explicabo aut quasi.
+        </p>
+        <b className="price">499.99</b>
+        <div className="filter">
+          <p>Select color</p>
+          <select>
+            <option>Black</option>
+            <option>Blue</option>
+            <option>Yellow</option>
+            <option>Red</option>
+          </select>
+          <p>Select size</p>
+          <select>
+            <option>XS</option>
+            <option>S</option>
+            <option>M</option>
+            <option>L</option>
+            <option>XL</option>
+          </select>
+        </div>
+        <div className="add">
+          <p>-</p>
+          <p>1</p>
+          <p>+</p>
+          <div />
+        </div>
+        <button className="button-1">Add to cart</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SelectedProduct
+export default SelectedProduct;
