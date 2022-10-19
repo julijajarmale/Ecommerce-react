2735324c341c94b88914c855d52c11bde0a53776
Julija Ilginytė-Jarmalė <julija.jarmale@gmail.com>
2022-10-19 13:15:48 +0300

    data.js

--- src/data.js
+++ src/data.js
@@ -67,39 +67,65 @@ export const products = [
     {
         id: 1,
         img: prod1,
+        title: 'Black and White dress',
+        price: '499.00 EUR',
+        color: ['black', 'white'],
+        
     
     },
     {
         id: 2,
         img: prod2,
+        title: 'Be a Star Co-ord suit',
+        price: '599.00 EUR',
+        color: ['black', 'white'],
     },
     {
         id: 3,
         img: prod3,
+        title: 'Shine Bright Co-ord suit',
+        price: '599.00 EUR',
+        color: ['red', 'white', 'black'],
         
     }, 
     
     {
         id: 4,
         img: prod4,
+        title: 'Be an Ocean Coat',
+        price: '299.00 EUR',
+        color: ['blue', 'white', 'black', 'beige'],
     
     },
     {
         id: 5,
         img: prod5,
+        title: 'Be YOU Co-ord set',
+        price: '199.00 EUR',
+        color: ['red', 'white', 'black', 'beige'],
     },
     {
         id: 6,
         img: prod6,
-    
+        title: 'Be a Sun Co-ord set',
+        price: '199.00 EUR',
+        color: ['yellow', 'white', 'red', 'black'],
     },
+    
+    
     {
         id: 7,
         img: prod7,
+        title: 'Burgundy coat',
+        price: '169.00 EUR',
+        color: ['burgundy', 'black'],
     },
     {
         id: 8,
         img: prod8,
+        title: ' Black coat',
+        price: '169.00 EUR',
+        color: ['burgundy', 'black'],
     
     },
    
